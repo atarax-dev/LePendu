b1fb226ac97587e5337643e1678c7aa18e484ef9
atarax-dev <83333973+atarax-dev@users.noreply.github.com>
2022-05-18 20:51:47 +0200

    Update settings.py

--- LePendu/settings.py
+++ LePendu/settings.py
@@ -27,12 +27,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(env("DEBUG", default=0))
+DEBUG = int(env('DEBUG', default=0))
 
-ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
